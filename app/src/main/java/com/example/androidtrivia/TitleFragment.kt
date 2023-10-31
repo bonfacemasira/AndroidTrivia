9adf87f27a9546e21da29599352ffc21d3850a26
bonfacemasira <bonfacemasira@gmail.com>
2023-10-31 19:47:38 +0300

    adding a Menu

--- app/src/main/java/com/example/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/example/androidtrivia/TitleFragment.kt
@@ -3,14 +3,19 @@ package com.example.androidtrivia
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.androidtrivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
+    @Suppress("DEPRECATION")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,12 +23,21 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
         )
-//        binding.playButton.setOnClickListener { view: View ->
-//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment3)
-//        }
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment4)
         )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
